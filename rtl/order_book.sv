4c4a4a7909c32386925e6b91234407cbf65f464a
Zavosh <zavosh76@yahoo.com>
2026-03-31 16:27:58 -0700

    Add assertion for price_level_t byte alignment in order_book module

--- rtl/order_book.sv
+++ rtl/order_book.sv
@@ -8,6 +8,8 @@ module order_book;
         assert (ob_pkg::RESTING_ORDER_PAD != 0)
             // $fatal(1, "ob_pkg: Comment out this line and uncomment next line and the _pad field in resting_order_t since padding is needed for byte alignment");
             else $fatal(1, "ob_pkg: Uncomment previous line and comment out this line and the _pad field in resting_order_t since no padding is needed for byte alignment");
+        assert ($bits(ob_pkg::price_level_t) % 8 == 0) // TEMPORARY: I need to add padding to a price_level_t wrapper to make it power of 2 bytes for L2.
+            else $fatal(1, "ob_pkg: price_level_t must be byte-aligned for Port B and C byte-enabled burst writes");
     end
 
 endmodule
